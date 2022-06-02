a6196173e6987ec3d1d4996c6b33eb4dca29ca37
amanjain2003 <92075107+amanjain2003@users.noreply.github.com>
2022-06-02 21:55:17 +0530

    Write a function to calculate area and perimeter of a rectangle.

---
+++ area_function.py
@@ -0,0 +1,10 @@
+def area(a, b):
+    return (a * b)
+ 
+def perimeter(a, b):
+    return (2 * (a + b))
+ 
+l = float(input("Enter length: ")) 
+b = float(input("Enter breadth: ")) 
+print ("Area = ", area(l, b))
+print ("Perimeter = ", perimeter(l, b))
