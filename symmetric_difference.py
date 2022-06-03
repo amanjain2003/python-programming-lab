aac9f8a2e510618f58e4350143ff425a5bf51918
amanjain2003 <92075107+amanjain2003@users.noreply.github.com>
2022-06-03 22:00:40 +0530

    Write a Python program to create a symmetric difference.

---
+++ symmetric_difference.py
@@ -0,0 +1,9 @@
+A = {'a', 'b', 'c', 'd'}
+B = {'c', 'd', 'e' }
+C = {}
+
+print(A.symmetric_difference(B))
+print(B.symmetric_difference(A))
+
+print(A.symmetric_difference(C))
+print(B.symmetric_difference(C))
